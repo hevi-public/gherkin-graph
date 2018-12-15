c6632d92bcce3c823195c5d1646fec49ae049ea2
Hevi <hevi.public@gmail.com>
2018-12-15 21:04:52 +0000

    full width / height

--- src/main/java/hu/hevi/gherkingraph/GherkinGraphApplication.java
+++ src/main/java/hu/hevi/gherkingraph/GherkinGraphApplication.java
@@ -1,12 +1,7 @@
 package hu.hevi.gherkingraph;
 
-import io.cucumber.messages.Messages;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class GherkinGraphApplication {
